7bc3b6a39ff6a8b9f4274f131f86b55a0b9957f0
George_Thomas <George_Thomas@student.mahoningctc.com>
2020-12-09 14:03:59 -0500

    up to Q1

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
 // Enter your code below to edit index.html
 
-yourName = "Ninjafiveo"
+document.title = "JS Output Lab";
+
+yourName = "Thomas"
 document.getElementById("yourName").innerHTML = yourName;
+
+// Write a short bio about yourself here.
+document.getElementById("myBio").innerHTML = "High schooler, programmer, & violinist";
+
+// What college do you want to go to?
+document.getElementById("collegeChoice").innerHTML = "YSU";
+
+// List the Program You want to study here.
+document.getElementById("programChoice").innerHTML = "Computer Science";
+
+// Write a short paragraph on why you chose that program and why the college of choice.
+document.getElementById("collegeReason").innerHTML = "I like using computers and making programs. YSU is nearby.";
+
+// What career title do you want to have?
+document.getElementById("careerChoice").innerHTML = "Web Developer";
+
+// What Company do you want to work for?
+document.getElementById("companyChoice").innerHTML = "Google";
+
+// Write a short paragraph on why you chose that career and/or company.
+document.getElementById("careerReason").innerHTML = "I like developing programs. Google is well known.";
+
+// Q1. What is the Document Object Model? (May require a little research)
+document.getElementById("answerOne").innerHTML = "The Document Object Model is an interface that displays XML or HTML documents as a tree structure. Each node of the tree is an object that represents a part of the document.";
+
+// Q2. What are the different ways Javascript can output to a web page? (May require a little research)
+document.getElementById("answerTwo").innerHTML = "...";
+
+// Q3. What HTML Elements can you manipulate or change with Javascript? (May require a little research)
+document.getElementById("answerThree").innerHTML = "...";
